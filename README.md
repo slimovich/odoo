3084e2452e90911fc12765f1d4e1fb5f4bf48709
slim <slim.baccar91@gmail.com>
2018-01-22 19:50:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Add Scroll Buttons When Tabs Overflow
---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tabs Overflow",
+    "version" : "1.0",
+    "author" : "Slim Baccar",
+    "category" : "Extra Tools",
+    "description": "Add scroll buttons when tabs overflow",
+    "summary": "Add scroll buttons when tabs overflow",
+    "data":[
+            "views/scroll_pages.xml",
+           ],
+
+    "qweb": ['static/src/xml/scroll_pages_template.xml',],
+    "images": ['static/src/description/banner.gif'],
+    "auto_install": False,
+    "installable": True
+}
